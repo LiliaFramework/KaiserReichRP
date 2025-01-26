55f93e926455421e41b435bb3b3ffa22efa01887
Samael <33399712+bleonheart@users.noreply.github.com>
2025-01-26 06:22:15 +0000

    Cleanup

--- kaiserreichrp/schema/schema.lua
+++ kaiserreichrp/schema/schema.lua
@@ -1,9 +1,4 @@
-﻿--[[--
-    Schema configuration options.
-
-    This is meant to define the Schema's default information.
-]]
-SCHEMA.name = "KaiserReichRP"
+﻿SCHEMA.name = "KaiserReichRP"
 SCHEMA.author = "76561198312513285"
 SCHEMA.discord = "@liliaplayer"
 SCHEMA.desc = "A KaiserReichRP .Schema"
